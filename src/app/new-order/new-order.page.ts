d8b496ffb7313cf79d69220ad33c0bc406374eff
Pedro Siqueira <pedro.isn1@gmail.com>
2020-01-27 23:16:14 -0300

    Alteracoes Pontuais

--- src/app/new-order/new-order.page.ts
+++ src/app/new-order/new-order.page.ts
@@ -97,4 +97,23 @@ async presentAlertSuccess() {
 
   }
 
+  formatPreco(){
+    let numberFormat : Intl.NumberFormatOptions = {
+      style : "currency",
+      currency : "BRL"
+    };
+
+    return this.preco.toLocaleString("pt-BR",numberFormat);
+  }
+
+  convertedDistance(distance: number){
+    if(distance < 1000){
+      return Math.round(distance).toString() + " m";
+    }
+    else {
+      return (distance/1000).toFixed(2).toString() + " Km";
+    }
+    
+  }
+
 }
